55dce29ec06fd83ff85641b381be178448248b6a
Thalia <thaliaiasmincr@gmail.com>
2021-04-25 02:54:40 -0300

    Aula 03 - in Orbit

---
+++ src/services/SettingsService.ts
@@ -0,0 +1,37 @@
+import { SettingsRepository } from "../repositories/SettingsRepository";
+import { getCustomRepository, Repository } from "typeorm"; 
+import { Setting } from "../entities/setting";
+
+interface ISettingsCreate {
+    chat: boolean;
+    username: string;
+}
+
+class SettingsService {
+    private settingsRepository: Repository<Setting>;
+
+    constructor() {
+        this.settingsRepository = getCustomRepository(SettingsRepository);
+    }
+
+    async create({ chat, username } : ISettingsCreate) {
+        const userAlreadyExists = await this.settingsRepository.findOne({
+            username,
+        });
+
+        if(userAlreadyExists) {
+            throw new Error("User already exists!")
+        }
+
+        const settings = this.settingsRepository.create({
+            chat,
+            username,
+        });
+
+        await this.settingsRepository.save(settings);
+
+        return settings 
+    }
+}
+
+export { SettingsService }
